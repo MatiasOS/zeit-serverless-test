188ce496d2b708aa436be87a5ee3c646b3a56a2b
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2020-01-16 16:06:16 -0300

    add some local deps

--- api/cool-face.js
+++ api/cool-face.js
@@ -1,9 +1,15 @@
 const cool = require('cool-ascii-faces');
+const { configObject, stringy } = require('../config');
 
 module.exports = async (req, res) => {
     const { body } = req;
     console.log('body ->', body);
     const secret = process.env.SECRET_VALUE;
 
-    res.status(200).json({ face: cool(), value: secret})
+    res.status(200).json({ 
+        face: cool(),
+        value: secret,
+        configObject,
+        stringy,
+    })
   }
