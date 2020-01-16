13bf7346187155e53327bcdb9ec7107491af1c4b
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2020-01-16 15:49:00 -0300

    logs change

--- api/cool-face.js
+++ api/cool-face.js
@@ -2,7 +2,7 @@ const cool = require('cool-ascii-faces');
 
 module.exports = async (req, res) => {
     const { body } = req;
-    console.log(body);
+    console.log('body ->', body);
     const secret = process.env.SECRET_VALUE;
 
     res.status(200).json({ face: cool(), value: secret})
